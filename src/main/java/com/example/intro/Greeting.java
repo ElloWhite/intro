46d2b96c99689c2cca98c0ce991a18186cf50eb3
ElloWhite <ebohemmanuel150@gmail.com>
2025-05-22 11:50:37 +0100

    added all files

---
+++ src/main/java/com/example/intro/Greeting.java
@@ -0,0 +1,38 @@
+package com.example.intro;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+    public class Greeting {
+
+        @Value("${country}")
+        String country;
+
+        @GetMapping("/getEnglishGreeting")
+        public String getEnglishGreetings(){
+            return "Good Morning";
+        };
+
+        @GetMapping("/getGhanaGreeting")
+        public String getGhanianGreetings(){
+            return "Ina Kuana";
+        };
+
+        @GetMapping("/getHausaGreeting")
+        public String getHausaGreetings(){
+            return country;
+        };
+
+        @GetMapping("/getAllGreeting")
+        public List<String> getAllGreetings(){
+            return List.of("Good morning", "Ina Kuana", "E Kaaro", "Ndewo");
+        };
+
+    }
+
+
